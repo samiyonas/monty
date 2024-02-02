a3bb7cfedd04f15a8d989598f0a50d42e49df5e4
samiyonas <samuel.x.yonas@gmail.com>
2024-02-02 10:53:10 -0800

    task 0

--- monty.c
+++ monty.c
@@ -39,7 +39,6 @@ int main(int argc, char **argv)
 			validity_check(&stack, operator, line_n);
 		}
 	}
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_n, operator);
 	fclose(file);
 	exit(EXIT_SUCCESS);
 }
