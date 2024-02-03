fdfc55cc622cb6a5f8059ec5f2682efaafcdace7
samiyonas <samuel.x.yonas@gmail.com>
2024-02-03 09:53:51 -0800

    nop

---
+++ task5.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: linked list
+ * @line_number: number of line
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+	UNUSED(line_number);
+}
