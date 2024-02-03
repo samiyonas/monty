75786b36c87749a1467c336d51989e64aa0d7f08
samiyonas <samuel.x.yonas@gmail.com>
2024-02-03 03:24:40 -0800

    c

--- monty.c
+++ monty.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
